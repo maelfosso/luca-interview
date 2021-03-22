c0e0351fba126cf1bce8c1bb0a524caf879ac7fd
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-03-22 20:33:04 +0100

    Publication service implemented

--- src/app/_services/publication.service.ts
+++ src/app/_services/publication.service.ts
@@ -1,9 +1,50 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Publication } from '../_models/publication';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicationService {
 
-  constructor() { }
+  private publicationsUrl = 'api/publications';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getPublications(): Observable<Publication[]> {
+    return this.http.get<Publication[]>(this.publicationsUrl)
+      .pipe(
+        catchError(this.handleError<Publication[]>('getPublications', []))
+      );
+  }
+
+  getPublication(id: number): Observable<Publication> {
+    const url = `${this.publicationsUrl}/${id}`;
+    return this.http.get<Publication>(url).pipe(
+      catchError(this.handleError<Publication>(`getPublication id=${id}`))
+    );
+  }
+
+  addPublication(publication: Publication): Observable<Publication> {
+    return this.http.post<Publication>(this.publicationsUrl, publication, this.httpOptions).pipe(
+      catchError(this.handleError<Publication>('addPublication'))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      console.error(error); 
+  
+      return of(result as T);
+    };
+  }
+
 }
