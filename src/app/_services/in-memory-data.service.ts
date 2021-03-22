34608b41671011bf57b1fe28e4fccb9513c36a21
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-03-22 20:28:17 +0100

    inMemoryData initialized

--- src/app/_services/in-memory-data.service.ts
+++ src/app/_services/in-memory-data.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import * as faker from 'faker';
+import { Publication } from '../_models/publication';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class InMemoryDataService {
+export class InMemoryDataService implements InMemoryDbService {
 
-  constructor() { }
+  createDb() {
+    const publications = Array(10).map((v, i) => {
+      return {
+        id: i + 10,
+        title: faker.lorem.sentence(),
+        publication: faker.lorem.paragraphs()
+      }
+    });
+    
+    return { publications };
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the publications array is empty,
+  // the method below returns the initial number (11).
+  // if the publications array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(publications: Publication[]): number {
+    return publications.length > 0 ? Math.max(...publications.map(hero => hero.id)) + 1 : 11;
+  }
+
 }
