f48840622f20d50a2789fe86504ed6f1afcae3ef
Mael FOSSO <fosso.mael.elvis@gmail.com>
2021-03-22 20:16:57 +0100

    http module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PublicationsComponent } from './publications/publications.component';
@@ -10,7 +11,8 @@ import { PublicationsComponent } from './publications/publications.component';
     PublicationsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
